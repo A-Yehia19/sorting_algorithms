ae5b551da47d1e554698dfb39c251949ec522a71
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-12-19 01:33:45 +0200

    init question

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order
+ * @array: The list to be printed
+ * @size: Number of elements in @array
+ * Return: NONE
+ */
+void quick_sort(int *array, size_t size)
+{
+	
+}
